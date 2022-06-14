a7abf73f945f70e62d1e334e57ef1b320a9e9b29
Redlion432 <russj027@hsd.k12.or.us>
2022-06-14 10:43:51 -0700

    Adding Ids for the Widgets

--- windchill_calculator/app .py
+++ windchill_calculator/app .py
@@ -2,14 +2,25 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
+
+# Generate unique ids for all widgets we want to change in runtime
+wind_id = dpg.generate_uuid()
+temp_id = dpg.generate_uuid()
+output_id = dpg.generate_uuid()
+
+def callback():
+    print("WindID: " + str(dpg.get_value(wind_id)))
+
 dpg.create_viewport(title='WindChill Calculator', width=600, height=300)
 
 with dpg.window(label="WindChill Calculator", width=600, height=300):
     dpg.add_text("Welcome to the WindChill Calculator")
-    dpg.add_input_int(label= "Wind Speed")
-    dpg.add_input_int(label= "Temperature")
-    dpg.add_button(label="Calculate")
+    dpg.add_input_int(label= "Wind Speed", width=100, tag=wind_id)
+    dpg.add_input_int(label= "Temperature", width=100, tag=temp_id)
+    dpg.add_button(label="Calculate", callback=callback)
     dpg.add_button(label="Clear")
+    dpg.add_text("", tag=output_id)
+
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
